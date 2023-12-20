5738513666d7b4be4e3856b3470be865b4ef8562
Amos Gionata Neri <a.neri@prowebconsulting.net>
2023-12-20 09:33:25 +0100

    detail page basic

--- src/routes/sensors/[id]/+page.svelte
+++ src/routes/sensors/[id]/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import {last_sensor_reads, sensor_reads, sensors} from "$lib/stores/store";
+    import {sensor_reads, sensors} from "$lib/stores/store";
     import {page} from '$app/stores';
     import {getContext, onDestroy, onMount} from "svelte";
     import type {Writable} from "svelte/store";
     import type {Websocket} from "$lib/Websocket/Websocket";
     import WebsocketListenEventEnum from "$lib/Enums/WebsocketListenEventEnum";
     import type SensorRead from "$lib/Models/SensorRead";
-    import Parser from "$lib/Parser/Parser";
-    import {goto} from "$app/navigation";
+    import Parser from "$lib/Parser/Parser.js";
+    import moment from "moment";
 
     let id = $page.params.id;
 
@@ -38,48 +38,37 @@
     });
 </script>
 
-<div class="my-4">
-    <button on:click={() => goto("/")}>HOME</button>
-</div>
+<div class="p-4">
+    <h1 class="text-3xl font-bold text-gray-600">Sensor ID: {id}</h1>
 
-<div>
-    <p>
-        <a href="/sensors">back</a>
-    </p>
+    <div class="m-4">
+        <a href="/" class="text-blue-500 hover:text-blue-700">Back to home</a>
+    </div>
 
-    <h1>Detail</h1>
-
-    <p>id: {id}</p>
-
-    {#if !$sensors[id]}
-        <p>sensor not found</p>
-    {:else}
-        <p>name: {$sensors[id].name}</p>
-        <p>ip address: {$sensors[id].ip_address}</p>
-        <p>created at: {$sensors[id].created_at}</p>
-        <p>latest
-            reading: {Parser.parseSensorReadValue($last_sensor_reads[id].sensor_value, $sensors[id].sensor_type)}</p>
-
-        <hr class="my-4">
-
-        <h2>Reads</h2>
-
-        <table>
-            <thead>
-            <tr>
-                <th>value</th>
-                <th>read at</th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each $sensor_reads as read}
-                <tr>
-                    <td class="mr-2">{Parser.parseSensorReadValue(read.sensor_value, $sensors[id].sensor_type)}</td>
-                    <td>{read.created_at}</td>
-                </tr>
-            {/each}
-            </tbody>
-        </table>
-    {/if}
-
+    <div class="grid grid-cols-1 gap-4 mx-8">
+        {#each $sensor_reads as read}
+            <div class="bg-white overflow-hidden shadow rounded-lg">
+                <div class="px-2 py-3">
+                    <dl>
+                        <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
+                            <dt class="text-sm font-medium text-gray-500">
+                                Value
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {Parser.parseSensorReadValue(read.sensor_value, $sensors[id].sensor_type)}
+                            </dd>
+                        </div>
+                        <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
+                            <dt class="text-sm font-medium text-gray-500">
+                                Date reading
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {moment(read.created_at).format('DD/MM/YYYY HH:mm:ss')}
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
