b1ffe4d320346482d3febe5500ba34bb72e9c3e1
Amos Gionata Neri <amosgionata.neri@gmail.com>
2023-12-22 12:11:22 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,31 @@ import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
 import {NodeGlobalsPolyfillPlugin} from "@esbuild-plugins/node-globals-polyfill";
 import {NodeModulesPolyfillPlugin} from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
     plugins: [sveltekit()],
     optimizeDeps: {
         esbuildOptions: {
+            // Node.js global to browser globalThis
             define: {
-                global: "globalThis",
+                global: 'globalThis'
             },
+            // Enable esbuild polyfill plugins
             plugins: [
                 NodeGlobalsPolyfillPlugin({
                     buffer: true,
+                    process: true,
                 }),
-                NodeModulesPolyfillPlugin(),
-            ],
-        },
+                NodeModulesPolyfillPlugin()
+            ]
+        }
     },
     build: {
         rollupOptions: {
             plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                }),
-                NodeModulesPolyfillPlugin(),
-            ],
-        },
-    },
+                rollupNodePolyFill()
+            ]
+        }
+    }
 });
